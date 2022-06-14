aa4457bef6b6bc4da3e742dfd38f4519f352b46a
Doruk <drktny@gmail.com>
2022-06-14 16:16:51 +0300

    Controllers added

---
+++ eTicketMVC/eTicketMVC/Controllers/ActorsController.cs
@@ -0,0 +1,24 @@
+﻿using eTicketMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicketMVC.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var actors = _context.Actors.ToList();
+            return View(actors);
+        }
+    }
+}
---
+++ eTicketMVC/eTicketMVC/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using eTicketMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicketMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return View(movies);
+        }
+    }
+}
